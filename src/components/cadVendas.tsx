23c2d51162c31e53ff38061153c91ff860c12468
Marcos Henrique <fernandes.jeronimo@outlook.com>
2024-07-03 00:13:50 -0300

    tela estoque

--- src/components/cadVendas.tsx
+++ src/components/cadVendas.tsx
@@ -150,7 +150,7 @@ const CadVendas: React.FC = () => {
   };
 
   const handleCancel = () => {
-    navigate('/');
+    navigate('/adm_user');
   };
 
   const toggleMenu = async (menuId: number) => {
